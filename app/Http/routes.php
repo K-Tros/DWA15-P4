0a1b4f1846dda677612eff826a5cde96cd2f84a2
Kevin Trosinski (Laptop) <krtrosinski@gmail.com>
2016-05-08 19:36:23 -0400

    Added auth checks for the routes where it is needed.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,8 +42,6 @@ Route::get('/search', function () {
     return view('search');
 });
 
-Route::post('/search', 'ComicController@postSearch');
-
 # Show login form
 Route::get('/login', 'Auth\AuthController@getLogin');
 
@@ -59,49 +57,6 @@ Route::get('/register', 'Auth\AuthController@getRegister');
 # Process registration form
 Route::post('/register', 'Auth\AuthController@postRegister');
 
-// not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
-Route::get('/wish-list/remove/{id?}', function($id = null) {
-    // comic for user and set wishlist to 0
-    $user = \Project4\User::where('id', '=', \Auth::id())->first();
-    $user->comics()->updateExistingPivot($id, ['wishlist'=> 0]);
-
-    // check if collection is also zero and detach if it is
-    $comics = $user->comics;
-    foreach ($comics as $comic) {
-        if ($comic->id == $id && $comic->pivot->collection == 0) {
-            $user->comics()->detach($id);
-        }
-    }
-
-    \Session::flash('message','Comic successfully removed from your wish list.');
-    return redirect('/wish-list');
-});
-
-// not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
-Route::get('/collection/remove/{id?}', function($id = null) {
-    // comic for user and set collection to 0
-    $user = \Project4\User::where('id', '=', \Auth::id())->first();
-    $user->comics()->updateExistingPivot($id, ['collection'=> 0]);
-
-    // check if wishlist is also zero and detach if it is
-    $comics = $user->comics;
-    foreach ($comics as $comic) {
-        if ($comic->id == $id && $comic->pivot->wishlist == 0) {
-            $user->comics()->detach($id);
-        }
-    }
-
-    \Session::flash('message','Comic successfully removed from your collection.');
-    return redirect('/collection');
-});
-
-// not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
-// TODO should probably try to find a way to change the add and remove methods to use a post and to stay on the same page
-Route::get('/collection/add/{id?}', 'ComicController@getCollectionAdd');
-
-// not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
-Route::get('/wish-list/add/{id?}', 'ComicController@getWishListAdd');
-
 Route::get('/practice', function() {
 
     $request = new Illuminate\Http\Request();
@@ -112,5 +67,51 @@ Route::get('/practice', function() {
 
     $comics = $helper->getComicsFromAPI($request);
     dd($comics);
-
 });
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::post('/search', 'ComicController@postSearch');
+
+    // not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
+    // TODO should probably try to find a way to change the add and remove methods to use a post and to stay on the same page
+    Route::get('/collection/add/{id?}', 'ComicController@getCollectionAdd');
+
+    // not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
+    Route::get('/wish-list/add/{id?}', 'ComicController@getWishListAdd');
+
+    // not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
+    Route::get('/wish-list/remove/{id?}', function($id = null) {
+        // comic for user and set wishlist to 0
+        $user = \Project4\User::where('id', '=', \Auth::id())->first();
+        $user->comics()->updateExistingPivot($id, ['wishlist'=> 0]);
+
+        // check if collection is also zero and detach if it is
+        $comics = $user->comics;
+        foreach ($comics as $comic) {
+            if ($comic->id == $id && $comic->pivot->collection == 0) {
+                $user->comics()->detach($id);
+            }
+        }
+
+        \Session::flash('message','Comic successfully removed from your wish list.');
+        return redirect('/wish-list');
+    });
+
+    // not really the best to use a get here, but kind of had to so that remove could work in an anchor tag
+    Route::get('/collection/remove/{id?}', function($id = null) {
+        // comic for user and set collection to 0
+        $user = \Project4\User::where('id', '=', \Auth::id())->first();
+        $user->comics()->updateExistingPivot($id, ['collection'=> 0]);
+
+        // check if wishlist is also zero and detach if it is
+        $comics = $user->comics;
+        foreach ($comics as $comic) {
+            if ($comic->id == $id && $comic->pivot->wishlist == 0) {
+                $user->comics()->detach($id);
+            }
+        }
+
+        \Session::flash('message','Comic successfully removed from your collection.');
+        return redirect('/collection');
+    });
+}
